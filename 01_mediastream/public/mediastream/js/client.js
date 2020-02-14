afe59092e8d49c4c7d193d3e62f07a847f0fab60
xuandongtang <xuandongtang@creditease.cn>
2020-02-14 11:07:23 +0800

    update: mediaStream 媒体调用

--- 01_mediastream/public/mediastream/js/client.js
+++ 01_mediastream/public/mediastream/js/client.js
@@ -1,45 +1,59 @@
+/**
+ * 在浏览器中访问音视频设备 核心API,返回Promise对象
+ * var promise = navigator.mediaDevices.getUserMedia(constraints);
+ * 然后将视频源赋给html5的video标签
+ */
+
 'use strict'
 
 var videoplay = document.querySelector('video#player');
 
-function gotMediaStream(stream){
+// 获得媒体流
+// 输入参数为MediaStream对象
+function gotLocalMediaStream(stream) {
 
     var videoTrack = stream.getVideoTracks()[0];
-
     window.stream = stream;
+
+    // 将它作为视频源赋值给 HTML5 的 video 标签的 srcObject 属性。
+    // 这样在 HTML 页面加载之后，就可以在该页面中看到摄像头采集到的视频数据了。
     videoplay.srcObject = stream;
 }
 
-function handleError(err){
+function handleError(err) {
     console.log('getUserMedia error:', err);
 }
 
 function start() {
 
-    if(!navigator.mediaDevices ||
-       !navigator.mediaDevices.getUserMedia){
-
+    //  判断是否能获取到媒体驱动以及用户的media
+    if (!navigator.mediaDevices ||
+        !navigator.mediaDevices.getUserMedia) {
         console.log('getUserMedia is not supported!');
         return;
-
-    }else{
-
-        var constraints = {
-            video : {
-                width: 640,	
-                height: 480,
-                frameRate:15,
-                facingMode: 'enviroment'
-		//,
-                //deviceId : deviceId ? {exact:deviceId} : undefined 
-            }, 
-            audio : false 
-        }
-
-        navigator.mediaDevices.getUserMedia(constraints)
-            .then(gotMediaStream)
-            .catch(handleError);
     }
+
+    // 初始化MediaStreamConstraints
+    // interface MediaStreamConstraints {
+    //     audio?: boolean | MediaTrackConstraints;
+    //     peerIdentity?: string;
+    //     video?: boolean | MediaTrackConstraints;
+    // }
+    // https://w3c.github.io/mediacapture-main/getusermedia.html#mediastreamconstraints
+    var constraints = {
+        video: {
+            width: 640,
+            height: 480,
+            frameRate: 15,//视频的帧率
+            facingMode: 'enviroment'
+        },
+        audio: false
+    }
+
+    // 核心代码
+    navigator.mediaDevices.getUserMedia(constraints)
+        .then(gotLocalMediaStream)
+        .catch(handleError);
 }
 
 start();
